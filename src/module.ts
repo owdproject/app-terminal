27ab792fcf97f5dab2e6e9e6cc01629d60c75313
dxlliv <dxlliv@proton.me>
2026-06-19 20:13:59 +0200

    refactor(terminal): migrated to defineDesktopModule and added default export

--- src/module.ts
+++ src/module.ts
@@ -1,12 +1,12 @@
 import {
-  defineNuxtModule,
   createResolver,
   addComponentsDir,
   addPlugin,
 } from '@nuxt/kit'
+import { defineDesktopModule } from '@owdproject/core/kit/authoring'
 import { registerTailwindPath } from '@owdproject/kit-tailwind/kit/registerTailwindPath'
 
-export default defineNuxtModule({
+export default defineDesktopModule({
   meta: {
     name: 'desktop-app-terminal',
     configKey: 'terminal',
@@ -15,14 +15,11 @@ export default defineNuxtModule({
     welcomeMessage: 'Welcome to Open Web Desktop!\nVersion: {desktopVersion} - owdproject.org\n\nTo get started, try using the \'help\' command\n\n',
     prompt: '$ ',
   },
-  async setup(options, nuxt) {
+  async setup(options: any, nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
     const coreVersion = nuxt.options.runtimeConfig.public.desktop?.coreVersion ?? '?.?'
     options.welcomeMessage = options.welcomeMessage.replace('{desktopVersion}', coreVersion)
-
-    nuxt.options.runtimeConfig.public.desktop ??= {}
-    nuxt.options.runtimeConfig.public.desktop.terminal = options
 
     addComponentsDir({
       path: resolve('./runtime/components'),
